dd1cc9cfa88ae5b9e1b287a1130a524fab9176ab
Michael Pope <popemray1986@gmail.com>
2018-08-11 20:12:46 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     if session[:name] = params[:name]
       redirect_to '/'
     else
-      redirect_to 'new'
+      redirect_to 'login'
     end
   end
 
